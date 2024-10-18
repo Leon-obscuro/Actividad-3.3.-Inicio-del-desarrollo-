04ea8e4c59eec8802b05cd429c153c09eadca211
Leon-obscuro <112833230+Leon-obscuro@users.noreply.github.com>
2024-10-17 23:14:20 -0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = "Hello World";
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
+      ),
+      body: Center(
+        child: Text(
+          appState.current,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter_windows_3.24.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.24.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app"
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.24.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app"
+  "FLUTTER_TARGET=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Leon\\Downloads\\Actividad 3.3. Inicio del desarrollo \\leo_app\\.dart_tool\\package_config.json"
+)
